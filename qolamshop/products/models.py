851f4d170046bfa457255f4873eb1cf0ab5f81db
Shaygan Askari <shaygan.askari@gmail.com>
2023-04-12 22:59:04 +0430

    ADD Seller model and ADD fields to Product model
    Added Seller class for the Product model and added it to a new 'seller'
    ForeignKey field in the Product model.
    Added rate, sale_count and delivery_delay fields to the Product model.

--- qolamshop/products/models.py
+++ qolamshop/products/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+class Seller(models.Model):
+    name = models.CharField(max_length=255, required=True, blank=False)
+    description = models.TextField(max_length=1275, required=False)
+    join_date = models.DateTimeField(auto_now_add=True)
+    rate = models.IntegerField(default=-1)
+    logo = models.ImageField(required=True)
+    sale_count = models.IntegerField(default=0)
+
+
 class Product(models.Model):
     """
     A model for each product in the shop
@@ -8,8 +17,12 @@ class Product(models.Model):
     name = models.CharField(max_length=255, required=True, blank=False)
     description = models.TextField(max_length=1275, required=False)
     price = models.FloatField(required=True, blank=False)
+    rate = models.IntegerField(default=-1)
+    delivery_delay = models.IntegerField(default=0)
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
     pub_date = models.DateTimeField(auto_now_add=True)
     logo = models.ImageField(required=True)
+    sale_count = models.IntegerField(default=0)
 
 
 class ProductImages(models.Model):
@@ -17,5 +30,5 @@ class ProductImages(models.Model):
     A model created for the Product model for it to be able to include multiple images
     """
 
-    product = models.ForeignKey(Product, related_name='images')
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
     image = models.ImageField(required=False)
