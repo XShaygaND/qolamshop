07a68aa06d9bf56f3981c5bff7b9ce097aeddcbb
Shaygan Askari <shaygan.askari@gmail.com>
2023-04-27 13:31:17 +0430

    Modify Seller, Product and ProductImages model
    -remove invalid 'required' arg from the models.
    -change rate field in the Seller and Product models to an FloatField
    instead of IntegerField.
    -change Product model's `logo` field name to `main_image`.
    -change ProductImages model name to ProductImage for better compatibility.
    -add `__str__` mehtods to Seller, Product and ProductImage models.
    -add `MEDIA_URL` and `MEDIA_ROOT` to `settings.py`.
    -add static_url to `qolamshop/urls.py`.
    -add test cases or each model
    -create migrations for the products app

--- qolamshop/products/models.py
+++ qolamshop/products/models.py
@@ -1,12 +1,57 @@
 from django.db import models
+from django.utils import timezone
+from django.core.exceptions import ValidationError
+
+
+def lower_spacify(text):
+    text = str(text)
+    return text.replace(' ', '_').lower()
+
+
+def validate_date_now(date):
+    """Raises ValidationError if the join date is any date other than the current time"""
+    if date != timezone.now():
+        raise ValidationError(
+            f'Join date must be set to today\'s date.\n{date}--{timezone.now()}')
+
+
+def validate_date_past(date):
+    """Raises ValidationError if the publish date is any date before the current time"""
+    if date < timezone.now():
+        raise ValidationError(
+            f'publish date must be today or in the future\n{date}--{timezone.now()}')
+
+
+def get_upload_path(instance, filename):
+    """
+    Returns appropriate url according to the model.
+
+    `{ls(instance.name)}/logo.png` if the instance is a Seller model
+    `{ls(instance.seller.name)}/{ls(instance.name)}/main_image.png` if the instance is a Product Model
+    `{ls(instance.product.seller.name)}/{ls(instance.product.name)}/image.png` if the instance is a ProductImage model
+    """
+    ls = lower_spacify
+    if isinstance(instance, Seller):
+        return f"{ls(instance.name)}/logo.png"
+    elif isinstance(instance, Product):
+        return f"{ls(instance.seller.name)}/{ls(instance.name)}/main_image.png"
+    return f"{ls(instance.product.seller.name)}/{ls(instance.product.name)}/image.png"
+
 
 class Seller(models.Model):
-    name = models.CharField(max_length=255, required=True, blank=False)
-    description = models.TextField(max_length=1275, required=False)
-    join_date = models.DateTimeField(auto_now_add=True)
-    rate = models.IntegerField(default=-1)
-    logo = models.ImageField(required=True)
+    """
+    A model for the sellers of the Product model, connected to which with a ForeignKey
+    """
+    name = models.CharField(max_length=255, blank=False, unique=True)
+    description = models.TextField(max_length=1275, blank=False)
+    join_date = models.DateTimeField(
+        auto_now_add=True, validators=[validate_date_now])
+    rate = models.FloatField(default=-1)
+    logo = models.ImageField(upload_to=get_upload_path, blank=False)
     sale_count = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return str(self.name)
 
 
 class Product(models.Model):
@@ -14,21 +59,29 @@ class Product(models.Model):
     A model for each product in the shop
     """
 
-    name = models.CharField(max_length=255, required=True, blank=False)
-    description = models.TextField(max_length=1275, required=False)
-    price = models.FloatField(required=True, blank=False)
-    rate = models.IntegerField(default=-1)
+    name = models.CharField(max_length=255, blank=False)
+    description = models.TextField(max_length=1275, blank=True)
+    price = models.FloatField(blank=False)
+    rate = models.FloatField(default=-1)
     delivery_delay = models.IntegerField(default=0)
     seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
-    pub_date = models.DateTimeField(auto_now_add=True)
-    logo = models.ImageField(required=True)
+    pub_date = models.DateTimeField(
+        auto_now_add=True, validators=[validate_date_past])
+    main_image = models.ImageField(upload_to=get_upload_path, blank=False)
     sale_count = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.name + ' | by: ' + self.seller.name
 
-class ProductImages(models.Model):
+
+class ProductImage(models.Model):
     """
     A model created for the Product model for it to be able to include multiple images
     """
 
-    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
-    image = models.ImageField(required=False)
+    product = models.ForeignKey(
+        Product, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=get_upload_path, blank=True)
+
+    def __str__(self):
+        return '<ProductImage>' + ' | for: ' + self.product.name
--- qolamshop/products/tests.py
+++ qolamshop/products/tests.py
@@ -1,3 +1,133 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.utils import timezone
 from django.test import TestCase
+from django.conf import settings
+from .models import Seller, Product, ProductImage
 
-# Create your tests here.
+
+def lower_spacify(text):
+    text = str(text)
+    return text.replace(' ', '_').lower()
+
+
+ls = lower_spacify
+media_root = settings.MEDIA_ROOT
+
+
+class SellerModelTests(TestCase):
+    """
+    Tests written specifically for the Seller model
+    """
+
+    def setUp(self):
+        """Creates a simple Seller model for testing"""
+        self.seller = Seller.objects.create(
+            name="Test Seller",
+            description="Test seller description",
+            logo=SimpleUploadedFile("logo.png", b"file_content"),
+        )
+
+    def tearDown(self):
+        """Deletes the test Seller model"""
+        self.seller.delete()
+
+    def test_seller_model_values(self):
+        """Test basic and default values of the Seller model"""
+        self.assertEqual(self.seller.name, 'Test Seller')
+        self.assertEqual(self.seller.description, "Test seller description")
+        self.assertTrue(timezone.now() - timezone.timedelta(seconds=3)
+                        < self.seller.join_date <= timezone.now())
+        self.assertEqual(self.seller.rate, -1)
+        self.assertEqual(self.seller.sale_count, 0)
+
+    def test_seller_model_logo_url(self):
+        """Test if the Seller's logo is saved in the right url"""
+        dir = f'/media/{self.seller.logo.name}'
+        self.assertEqual(self.seller.logo.url, dir)
+
+
+class ProductImageModelTests(TestCase):
+    """
+    Tests written specifically for the ProductImage model
+    """
+
+    def setUp(self):
+        """Creates a simple Product and Seller model for testing"""
+        self.product = Product.objects.create(
+            name='Test product',
+            price=10.99,
+            seller=Seller.objects.create(
+                name="Test Seller",
+                description="Test seller description",
+                logo=SimpleUploadedFile("img.png", b"file_content"),
+            ),
+            main_image=SimpleUploadedFile("img", b"file_content"),
+        )
+
+    def tearDown(self):
+        """Deletes the test Product and Seller models"""
+        self.product.seller.delete()
+        self.product.delete()
+
+    def test_productimage_model_multiple_product_images(self):
+        """Test that a product can have multiple images"""
+        # Create multiple ProductImages instances for testing
+        for i in range(1, 4):
+            ProductImage.objects.create(
+                product=self.product,
+                image=SimpleUploadedFile(f"img_{i}.png", b"file_content"),
+            )
+
+        # Test that the product has the expected number of images
+        self.assertEqual(self.product.images.count(), 3)
+
+    def test_productimage_model_image_url(self):
+        """Test if the ProductImage image is saved in the right url"""
+        ProductImage.objects.create(
+            product=self.product, image=SimpleUploadedFile(f"img.png", b"file_content"))
+        dir = f'/media/{self.product.images.all()[0].image.name}'
+        self.assertEqual(self.product.images.all()[0].image.url, dir)
+
+
+class ProductModelTests(TestCase):
+    """
+    Tests written specifically for the Product model
+    """
+
+    def setUp(self):
+        """Creates a simple Product and Seller model for testing"""
+        self.product = Product.objects.create(
+            name='Test product',
+            price=10.99,
+            seller=Seller.objects.create(
+                name="Test Seller",
+                description="Test seller description",
+                logo=SimpleUploadedFile("img.png", b"file_content"),
+            ),
+            main_image=SimpleUploadedFile("img", b"file_content"),
+        )
+
+    def tearDown(self):
+        """Deletes the test Product and Seller models"""
+        self.product.seller.delete()
+        self.product.delete()
+
+    def test_product_model_values(self):
+        """Test basic and default values of the Product model"""
+        self.assertEqual(self.product.name, 'Test product')
+        self.assertEqual(self.product.price, 10.99)
+        self.assertEqual(self.product.seller,
+                         Seller.objects.get(name='Test Seller'))
+        self.assertEqual(self.product.description, '')
+        self.assertFalse(self.product.description)
+        self.assertTrue(timezone.now() - timezone.timedelta(seconds=5)
+                        < self.product.pub_date <= timezone.now())
+        self.assertEqual(self.product.rate, -1)
+        self.assertEqual(self.product.delivery_delay, 0)
+        self.assertEqual(self.product.sale_count, 0)
+        self.assertEqual(self.product.images.count(), 0)
+
+    def test_product_model_main_image_url(self):
+        """Test if the Product's main_image is saved in the right url"""
+        dir = f'/media/{self.product.main_image.name}'
+        self.assertEqual(self.product.main_image.url, dir)
